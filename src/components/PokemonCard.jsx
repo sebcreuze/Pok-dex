282091a3b0cd03d131943f186b1dad4d095bec84
unknown <sebastien.creuze@live.fr>
2024-03-27 16:29:21 +0100

    finish quest 5

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function PokemonCard({ pokemon }) {
   return (
     <figure>
@@ -15,5 +17,12 @@ function PokemonCard({ pokemon }) {
     </figure>
   );
 }
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }),
+};
 
 export default PokemonCard;
