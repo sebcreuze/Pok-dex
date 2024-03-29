c0a49f3c1cae024193cd0c4e49393bd74acfaee5
unknown <sebastien.creuze@live.fr>
2024-03-29 12:31:43 +0100

    finish quest 8

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,18 @@
 import PropTypes from "prop-types";
 
-export default function NavBar({ pokemonIndex, pokemonList, setpokemonIndex }) {
-  const handleClickSuivant = () => {
-    setpokemonIndex(pokemonIndex + 1);
-  };
-  const handleClickPrécédent = () => {
-    setpokemonIndex(pokemonIndex - 1);
-  };
+export default function NavBar({ pokemonList, setpokemonIndex }) {
   return (
     <>
-      {pokemonIndex > 0 && (
-        <button onClick={handleClickPrécédent}>
-          <p>Précédent</p>
+      {pokemonList.map((pokemon, index) => (
+        <button key={pokemon.name} onClick={() => setpokemonIndex(index)}>
+          {pokemon.name}
         </button>
-      )}
-
-      {pokemonIndex < pokemonList.length - 1 && (
-        <button onClick={handleClickSuivant}>
-          <p>Suivant</p>
-        </button>
-      )}
+      ))}
     </>
   );
 }
 
 NavBar.protoTypes = {
-  pokemonIndex: PropTypes.number.isRequired,
   setpokemonIndex: PropTypes.func.isRequired,
   pokemonList: PropTypes.arrayOf(
     PropTypes.shape({
